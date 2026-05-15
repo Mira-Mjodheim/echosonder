4514299b86613486ad0cb3d44568aa04c37b8787
Mira <mira.mjodheim.be@gmail.com>
2026-05-15 20:23:27 +0200

    fix: minimal babel config without missing plugins

--- babel.config.js
+++ babel.config.js
@@ -1,11 +1,5 @@
 module.exports = {
   presets: [
-    ['@babel/preset-env', { targets: { browsers: ['>= browserslist@1.53.0'] } }],
-    '@babel/preset-react'
+    ['@babel/preset-env', { targets: { node: 'current' } }],
   ],
-  plugins: [
-    '@babel/plugin-proposal-class-properties',
-    '@babel/plugin-proposal-object-rest-spread',
-    '@babel/plugin-transform-runtime'
-  ]
 };
