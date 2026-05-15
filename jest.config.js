1dd7e8fc291641f0b89cf526c99cc2efead6b234
Mira <mira.mjodheim.be@gmail.com>
2026-05-15 20:22:25 +0200

    fix: proper Jest config (strip invalid markdown fences, remove ts-jest/jsdom)

--- jest.config.js
+++ jest.config.js
@@ -1,25 +1,5 @@
-```javascript
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'jsdom',
-  collectCoverage: true,
-  coverageDirectory: 'coverage',
-  coverageReporters: ['json', 'lcov', 'clover'],
-  moduleFileExtensions: ['js', 'jsx', 'json'],
-  moduleDirectories: ['node_modules', 'src'],
-  transform: {
-    '^.+\\.(js|jsx)$': 'babel-jest',
-  },
-  transformIgnorePatterns: [
-    '/node_modules/(?!(react|react-dom)/).+\\.js$',
-  ],
-  setupFilesAfterEnv: ['<rootDir>/setupTests.js'],
-  testPathIgnorePatterns: [
-    '/node_modules/',
-    '\\.db\\.js',
-    '\\.config\\.js',
-  ],
-  moduleNameMapper: {
-    '\\.(css|less|scss)$': 'identity-obj-proxy',
-  },
+  testEnvironment: 'node',
+  testPathPattern: 'tests/server',
+  testTimeout: 15000,
 };
